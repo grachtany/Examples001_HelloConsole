1ba94f5912fab7b3a3ef0e16bb1c164cdda76bed
tatangracheva <grachtany@yandex.ru>
2022-11-21 16:44:07 +0300

    Random

---
+++ Examples003_Summ/Program.cs
@@ -0,0 +1,7 @@
+﻿int NumberA = new Random().Next(1,10) ;// 1 2 3 ..9
+Console.WriteLine (NumberA);
+int NumberB = new Random().Next(1,10) ;// 1 2 3 ..9
+Console.WriteLine (NumberA);
+int result = NumberA + NumberB;
+Console.WriteLine (NumberA +NumberB);
+Console.WriteLine (result);
---
+++ Examples004_SummDel/Program.cs
@@ -0,0 +1,5 @@
+﻿double NumberA = 12;
+double NumberB = 5;
+double result = NumberA / NumberB;
+Console.WriteLine (NumberA / NumberB);
+Console.WriteLine (result);
