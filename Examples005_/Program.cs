e3270480e737aa439afdd0e2d0c381d6eb8061b7
tatangracheva <grachtany@yandex.ru>
2022-11-21 16:57:41 +0300

    приветствие люимчика

---
+++ Examples005_/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "таня")
+{
+    Console.WriteLine ("Ура, Приветствую тебя Таня!");
+}
+else
+{
+    Console.Write ("Привет, ");
+    Console.Write (username);
+}
